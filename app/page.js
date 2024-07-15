4796dddc43d02c8f4c8c2e251adacdf54156fb0e
cahyo988 <cahyos493@gmail.com>
2024-07-15 18:25:14 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,247 @@
+"use client"
+
 import Image from "next/image";
+import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai'
+import myProfile from '../public/myProfile.png'
+import design from '../public/design.png'
+import framework from '../public/framework.png'
+import code from '../public/code.png'
+import web1 from "../public/web1.jpg"
+import web2 from "../public/web2.jpg"
+import web3 from "../public/web3.jpg"
+import web4 from "../public/web4.jpg"
+import web5 from "../public/web5.jpg"
+
+import { FaFigma } from "react-icons/fa";
+import { SiCanva } from "react-icons/si";
+import { AiOutlineHtml5 } from "react-icons/ai";
+import { FaCss3 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { RiNextjsLine } from "react-icons/ri";
+import { FaLaravel } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaPhp } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa";
+import { DiMongodb } from "react-icons/di";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { useState } from "react";
+
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={darkMode ? "dark" : ""} >
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900 dark:text-slate-300 poppins">
+        <section className="min-h-screen">
+          <nav className="py-5 mb-12 flex justify-between bg-slate ">
+            <h1 className="text-xl fraunces">cahyoDEV</h1>
+            <ul className="flex items-center">
+              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" /></li>
+              <li>
+                <a className="bg-gradient-to-b from-cyan-500 to-teal-500 text-white px-4 py-2 rounded ml-5" href="">Resume</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 ">
+            <h2 className="text-4xl py-2 text-teal-700 font-medium md:text-6xl">Cahyo Saputra</h2>
+            <h3 className="text-2xl py-2 md:text-3xl">Developer And Designer...</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-slate-300">
+              Freelancer providing services for programming and design content needs. Join me down bellow and let's get cracking!
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-14 py-3 text-gray-600">
+            <a href="https://github.com/cahyo988"><FaGithub/></a>
+            <a href="https://www.linkedin.com/in/cahyo-saputra-a26411204/"><AiFillLinkedin /></a>
+            <a href="https://www.instagram.com/saputracah_/"><FaSquareInstagram /></a>
+            
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          </div>
+          <div className="relative bg-gradient-to-b from-teal-400 rounded-full w-80 h-80 mt-10 mx-auto overflow-hidden">
+            <Image src={myProfile} layout="fill" objectFit="cover" />
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        {/* Services */}
+        <section>
+          <div>
+            <h3 className="text-3xl py-5 ">
+              Services I Offer
+            </h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-300">
+              Since the beginning of my jurney as a freelance developer, I've done remote work for some <span className="text-teal-500">Customer</span> and colaborated with some <span className="text-teal-500">People</span>
+            </p>
+            <p className="text-md py-2 leading-8  text-gray-800 dark:text-slate-300">
+              I Offer from a wide range of services, Including programming and design.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="basis-1/2 text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700">
+              <Image src={design} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Design
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <div className="grid grid-cols-1 md:grid-cols-1 gap-2 p-5">
+                <div className="text-gray-800 py-1 dark:text-slate-300 flex gap-3 justify-center">
+                  <span className="py-1 text-4xl"><FaFigma /></span>
+                  <p className="py-2">Figma</p>
+                </div>
+                <div className="text-gray-800 py-1 dark:text-slate-300 flex gap-3 justify-center">
+                  <span className="py-1 text-4xl"><SiCanva /></span>
+                  <p className="py-2">Canva</p>
+                </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              </div>
+            </div>
+            <div className="basis-1/2 text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700">
+              <Image src={code} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Programming Language
+              </h3>
+              <p className="py-2">
+                The programming language I use to code
+              </p>
+              <h4 className="py-4 text-teal-600">
+                Programming Language i use
+              </h4>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-5 p-5">
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><AiOutlineHtml5 /></span>
+                  <p className="text-sm md:text-xs">HTML</p>
+                </div>
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><FaCss3 /></span>
+                  <p className="text- md:text-xs">CSS</p>
+                </div>
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><IoLogoJavascript /></span>
+                  <p className="text-sm md:text-xs">JS</p>
+                </div>
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><RiTailwindCssFill /></span>
+                  <p className="text-sm md:text-xs">Tailwind</p>
+                </div>
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><FaPhp /></span>
+                  <p className="text-sm md:text-xs">PHP</p>
+                </div>
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><FaBootstrap /></span>
+                  <p className="text-sm md:text-xs">Bootstrap</p>
+                </div>
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><FaReact /></span>
+                  <p className="text-sm md:text-xs">React</p>
+                </div>
+                <div className="text-gray-800 dark:text-slate-300 flex flex-col items-center">
+                  <span className="text-4xl mb-2 md:text-3xl"><DiMongodb /></span>
+                  <p className="text-sm md:text-xs">MongoDB</p>
+                </div>
+              </div>
+            </div>
+            <div className="basis-1/2 text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-700">
+              <Image src={framework} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Programming Framework
+              </h3>
+              <p className="py-2">
+                The framework I use to create projects
+              </p>
+              <h4 className="py-4 text-teal-600">Coding Framework I Use</h4>
+              <div className="grid grid-cols-1 md:grid-cols-1 gap-2 p-5">
+                <div className="text-gray-800 py-1 dark:text-slate-300 flex gap-3 justify-center">
+                  <span className="py-1 text-4xl"><RiNextjsLine /></span>
+                  <p className="py-2">Next.js</p>
+                </div>
+                <div className="text-gray-800 py-1 dark:text-slate-300 flex gap-3 justify-center">
+                  <span className="py-1 text-4xl"><FaLaravel />
+                  </span>
+                  <p className="py-2">Laravel</p>
+                </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              </div>
+            </div>
+          </div>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section>
+          <div>
+            <h3 className="text-3xl py-">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-400">
+              With expertise in <span className="text-teal-500"> Programming</span>, I deliver innovative digital solutions that are structured and efficient. This portfolio showcases my dedication in turning concepts into robust implementations. I hope this portfolio inspires and showcases <span className="text-teal-500"> my journey</span>  and the digital works I have produced.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web1}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web2}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web3}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web4}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web5}
+              />
+            </div>
+          </div>
+
+
+        </section>
+      </main>
+    </div>
+
   );
 }
