3a3ac6721ac02743e1a0fd30b6a4cec93c62851b
Cahyo Saputra <73044311+cahyo988@users.noreply.github.com>
2025-11-18 13:04:36 +0700

    Remove service worker registration and add script

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Poppins, Fraunces } from "next/font/google";
 import "./globals.css";
-import SWRegister from "./sw-register";
+import Script from "next/script";
 
 const poppins_init = Poppins({
   subsets: ["latin"],
@@ -26,9 +26,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="monetag-onclick" strategy="afterInteractive">
+          {`(function(s){s.dataset.zone='10202962',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));`}
+        </Script>
+      </head>
+
       <body className={`${poppins_init.variable} ${fraunces_init.variable}`}>
-        {/* Register service worker */}
-        <SWRegister />
         {children}
       </body>
     </html>
